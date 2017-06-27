8b1e6c91c46f15b6f3fd764d47fd7dc89c66a04c
Eric Wang <ewang1997@gmail.com>
2017-06-27 15:16:40 -0700

    separate style sheets

--- src/styles/index.js
+++ src/styles/index.js
@@ -5,4 +5,7 @@ import './bootstrap/bootstrap.scss';
 require("mapbox-gl/dist/mapbox-gl.css");
 require("@mapd/mapdc/mapdc.css");
 require("@mapd/mapdc/scss/chart.scss");
+
 import './style.scss'
+import './tweetSidebar.scss'
+import './legend.scss'
